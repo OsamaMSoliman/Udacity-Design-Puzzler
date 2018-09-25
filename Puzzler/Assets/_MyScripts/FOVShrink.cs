8289400f9d245c6c2fd9836c20879dd973f65ead
Nsr <es-osama.nsr1318@alexu.edu.eg>
2018-09-26 00:57:04 +0200

    added post processing stack

---
+++ Puzzler/Assets/_MyScripts/FOVShrink.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class FOVShrink : MonoBehaviour {
+
+	public float simulatedFOV;
+
+	public float IPD;
+	public float SCREEN_DISTANCE;
+
+	public PostProcessingBehaviour postProcess;
+
+	private VignetteModel vModel;
+	private float         maxFOV;
+
+	// Use this for initialization
+	void Start () {
+		vModel = postProcess.profile.vignette;
+		maxFOV = Mathf.Rad2Deg * Mathf.Atan2(IPD / 2f, SCREEN_DISTANCE);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		VignetteModel.Settings settings = vModel.settings;
+		settings.intensity = 1f - Mathf.Clamp01(simulatedFOV / maxFOV);
+		vModel.settings    = settings;
+	}
+}
